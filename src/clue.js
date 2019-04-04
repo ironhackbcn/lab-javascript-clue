9767f0b8cab248c367b013c1f0d00ce6e9c26ac7
wervux <mypersonalemail@email.com>
2019-04-04 09:24:21 +0200

    forgot to format

--- src/clue.js
+++ src/clue.js
@@ -1,137 +1,146 @@
 // Characters
 var mrGreen = {
-    first_name:   'Jacob',
-    last_name:    'Green',
-    color:        'green',
-    description:  'He has a lot of connections',
-    age:          '45',
-    image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-    occupation:   'Entrepreneur'
+    first_name: 'Jacob',
+    last_name: 'Green',
+    color: 'green',
+    description: 'He has a lot of connections',
+    age: '45',
+    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    occupation: 'Entrepreneur'
 }
 
 var drOrchid = {
-    first_name:   'Doctor',
-    last_name:    'Orchid',
-    color:        'white',
-    description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
-    age:          '26',
-    image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    occupation:   'Scientist'
+    first_name: 'Doctor',
+    last_name: 'Orchid',
+    color: 'white',
+    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    age: '26',
+    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    occupation: 'Scientist'
 }
 
 var profPlum = {
-    first_name:   'Victor',
-    last_name:    'Plum',
-    color:        'purple',
-    description:  'Billionare video game designer',
-    age:          '22',
-    image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
-    occupation:   'Designer'
+    first_name: 'Victor',
+    last_name: 'Plum',
+    color: 'purple',
+    description: 'Billionare video game designer',
+    age: '22',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+    occupation: 'Designer'
 }
 
 var missScarlet = {
-    first_name:   'Kasandra',
-    last_name:    'Scarlet',
-    color:        'red',
-    description:  'She is an A-list movie star with a dark past',
-    age:          '31',
-    image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
-    occupation:   'Actor'
+    first_name: 'Kasandra',
+    last_name: 'Scarlet',
+    color: 'red',
+    description: 'She is an A-list movie star with a dark past',
+    age: '31',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+    occupation: 'Actor'
 }
 
 var mrsPeacock = {
-    first_name:   'Eleanor',
-    last_name:    'Peacock',
-    color:        'blue',
-    description:  'She is from a wealthy family and uses her status and money to earn popularity',
-    age:          '36',
-    image:        'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-    occupation:   'Socialité'
+    first_name: 'Eleanor',
+    last_name: 'Peacock',
+    color: 'blue',
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    age: '36',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    occupation: 'Socialité'
 }
 
 var mrMustard = {
-    first_name:   'Jack',
-    last_name:    'Mustard',
-    color:        'yellow',
-    description:  'He is a former football player who tries to get by on his former glory',
-    age:          '62',
-    image:        'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
-    occupation:   'Retired Football player'
-} 
+    first_name: 'Jack',
+    last_name: 'Mustard',
+    color: 'yellow',
+    description: 'He is a former football player who tries to get by on his former glory',
+    age: '62',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
+    occupation: 'Retired Football player'
+}
 
 // Weapons
-var rope = { 
+var rope = {
     name: 'rope',
-    weight: 10 }
-var knife = { 
+    weight: 10
+}
+var knife = {
     name: 'knife',
-    weight: 8 }
-var candlestick = { 
+    weight: 8
+}
+var candlestick = {
     name: 'candlestick',
-    weight: 2 }
-var dumbbell = { 
+    weight: 2
+}
+var dumbbell = {
     name: 'dumbbell',
-    weight: 30 }
-var poison = { 
+    weight: 30
+}
+var poison = {
     name: 'poison',
-    weight: 2 }
-var axe = { 
+    weight: 2
+}
+var axe = {
     name: 'axe',
-    weight: 15 }
-var bat = { 
+    weight: 15
+}
+var bat = {
     name: 'bat',
-    weight: 13 }
-var trophy = { 
+    weight: 13
+}
+var trophy = {
     name: 'trophy',
-    weight: 25 }
-var pistol = { 
+    weight: 25
+}
+var pistol = {
     name: 'pistol',
-    weight: 20 }
+    weight: 20
+}
 
 // Rooms
-var dinningRoom = { 
+var dinningRoom = {
     name: 'DinningRoom'
 }
-var conservatory = { 
+var conservatory = {
     name: 'Conservatory'
 }
-var kitchen = { 
+var kitchen = {
     name: 'Kitchen'
 }
-var study = { 
+var study = {
     name: 'Study'
 }
-var library = { 
+var library = {
     name: 'Library'
 }
-var billiardRoom = { 
+var billiardRoom = {
     name: 'BilliardRoom'
 }
-var lounge = { 
+var lounge = {
     name: 'Lounge'
 }
-var ballroom = { 
+var ballroom = {
     name: 'Ballroom'
 }
-var hall = { 
+var hall = {
     name: 'Hall'
 }
-var spa = { 
+var spa = {
     name: 'Spa'
 }
-var livingRoom = { 
+var livingRoom = {
     name: 'LivingRoom'
 }
-var observatory = { 
+var observatory = {
     name: 'Observatory'
 }
-var theater = { 
+var theater = {
     name: 'Theater'
 }
-var guestHouse = { 
+var guestHouse = {
     name: 'GuestHouse'
 }
-var patio = { 
+var patio = {
     name: 'Patio'
 }
 
