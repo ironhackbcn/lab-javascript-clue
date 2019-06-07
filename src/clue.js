7821a27819faa6da774817cbbeea1c16a6c1ca97
Mario Espada <mario.espada77@gmail.com>
2019-06-07 03:21:38 +0200

    functions

--- src/clue.js
+++ src/clue.js
@@ -84,11 +84,162 @@ name: Guest House
 name: Patio
 */
 
+let mrGreen = {
+  first_name: "Jacob",
+  last_name: "Green",
+  color: "green",
+  description: "He has a lot of connections",
+  age: 45,
+  image: (src =
+    "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg"),
+  occupation: "Entrepreneur"
+};
+
+let drOrchid = {
+  first_name: "Doctor",
+  last_name: "Orchid",
+  color: "white",
+  description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+  age: 26,
+  image: (src = "http://www.radiotimes.com/uploads/images/Original/111967.jpg"),
+  occupation: "Scientist"
+};
+
+let profPlum = {
+  first_name: "Victor",
+  last_name: "Plum",
+  color: "purple",
+  description: "Billionare video game designer",
+  age: 22,
+  image: (src =
+    "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg"),
+  occupation: "Designer"
+};
+
+let missScarlet = {
+  first_name: "Kasandra",
+  last_name: "Scarlet",
+  color: "red",
+  description: "She is an A-list movie star with a dark past",
+  age: 31,
+  image: (src =
+    "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg"),
+  occupation: "Actor"
+};
+
+let mrsPeacock = {
+  first_name: "Eleanor",
+  last_name: "Peacock",
+  color: "blue",
+  description:
+    "She is from a wealthy family and uses her status and money to earn popularity",
+  age: 36,
+  image: (src = "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg"),
+  occupation: "Socialité"
+};
+
+let mrMustard = {
+  first_name: "Jack",
+  last_name: "Mustard",
+  color: "yellow",
+  description:
+    "He is a former football player who tries to get by on his former glory",
+  age: 62,
+  image: (src =
+    "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg"),
+  occupation: "Retired Football player"
+};
+
+let rope = {
+  name: "rope",
+  weight: 10
+};
+
+let knife = {
+  name: "knife",
+  weight: 8
+};
+
+let candlestick = {
+  name: "dumbbell",
+  weight: 2
+};
+
+let dumbbell = {
+  name: "dumbbell",
+  weight: 30
+};
+
+let poison = {
+  name: "poison",
+  weight: 2
+};
+
+let axe = {
+  name: "axe",
+  weight: 15
+};
+
+let bat = {
+  name: "trophy",
+  weight: 13
+};
+
+let trophy = {
+  name: "trophy",
+  weight: 25
+};
+
+let pistol = {
+  name: "pistol",
+  weight: 20
+};
+
 // Characters Collection
-var charactersArray = [];
+let charactersArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock];
 
 // Rooms' Collection
-var roomsArray = [];
+let roomsArray = [
+  "Dinning Room",
+  "Conservatory",
+  "Kitchen",
+  "Study",
+  "Library",
+  "Billiard Room",
+  "Lounge",
+  "Ballroom",
+  "Hall",
+  "Spa",
+  "Living Room",
+  "Observatory",
+  "Theater",
+  "Guest House",
+  "Patio"
+];
 
 // Weapons Collection
-var weaponsArray = [];
+let weaponsArray = [
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol
+];
+
+//Define random selector
+
+function randomSelector(cards) {
+  return cards[Math.floor(Math.random() * cards.length)];
+}
+function pickMistery() {
+  let mistery = [" "];
+
+  mistery[0] = randomSelector(charactersArray);
+  mistery[2] = randomSelector(roomsArray);
+  mistery[1] = randomSelector(weaponsArray);
+  return mistery;
+}
