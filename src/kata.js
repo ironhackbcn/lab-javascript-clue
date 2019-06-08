e58b7e52e7d1b6de82748f04543f1b727d22d2d1
Ale Bausa <ale.bausa@gmail.com>
2019-06-08 13:15:21 +0200

    done

---
+++ src/kata.js
@@ -0,0 +1,11 @@
+function divisibleBy(array, divisor){
+   array.forEach(function(element){
+    var result = element / divisor;
+    var test = Number.isInteger(divide);
+    var newArray = [];
+    if (test === true){
+    newArray.push(divide);
+    }
+  });
+  return new
+}
