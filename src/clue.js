5cc34dae3df767c22141a377137f081e30f2759f
Gloria Visus Margarit <gloriavisusmargarit@MacBook-Air-de-Gloria.local>
2019-06-26 16:09:42 +0200

    clue updated

--- src/clue.js
+++ src/clue.js
@@ -1,94 +1,144 @@
 // Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
+var charactersArray = [
+{ name: "mrGreen",
+  data: [{ 
+    first_name:  "Jacob",
+    last_name:    "Green",
+    color:        "green",
+    description:  "He has a lot of connections",
+    age:          45,
+    image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation:   "Entrepreneur"}
+]},
 
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-occupation:   Scientist
+{  name: "drOrchid",
+    data: [{
+    first_name:   "Doctor",
+    last_name:    "Orchid",
+    color:        "white",
+    description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    age:          26,
+    image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation:   "Scientist"
 
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionare video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
+}]},
 
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
+{  name: "profPlum",
+    data: [{
+    first_name:   "Victor",
+    last_name:    "Plum",
+    color:        "purple",
+    description:  "Billionare video game designer",
+    age:          22,
+    image:        "hhttps://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+    occupation:   "Designer"
 
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
+}]},
 
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-occupation:   Retired Football player
+{  name: "missScarlet",
+    data: [{
+    first_name:   "Kasandra",
+    last_name:    "Scarlet",
+    color:        "red",
+    description:  "She is an A-list movie star with a dark past",
+    age:          31,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+    occupation:   "Actor"
+
+}]},
+
+
+{  name: "mrsPeacock",
+    data: [{
+    first_name:   "Eleanor",
+    last_name:    "Peacock",
+    color:        "blue",
+    description:  "She is from a wealthy family and uses her status and money to earn popularity",
+    age:          36,
+    image:        " https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation:   "Socialité"
+
+}]},
+
+{  name: "mrMustard",
+    data: [{
+    first_name:   "Jack",
+    last_name:    "Mustard",
+    color:        "yellow",
+    description:  "He is a former football player who tries to get by on his former glory",
+    age:          62,
+    image:        " https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation:   "Retired Football player"
+
+}]
+
+}];
 
 // Weapons
+var weaponsArray = [
+{ name: "rope",
+    data:[{ weight: 10 }] },
+{ name: "knife",
+    data:[{ weight: 8 }] },   
+{ name: "candlestick",
+    data:[{ weight: 2 }] }, 
+{ name: "dumbbell",
+    data:[{ weight: 30 }] },
+{ name: "poisom",
+    data:[{ weight: 2 }] }, 
+{ name: "axe",
+    data:[{ weight: 15 }] },  
+{ name: "bat",
+    data:[{ weight: 13 }] }, 
+{ name: "trophy",
+    data:[{ weight: 25 }] },  
+{ name: "pistol",
+    data:[{ weight: 20 }] }
 
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
+];
 
 // Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-*/
+var roomsArray = [{
 
-// Characters Collection
-var charactersArray = [];
+name: "Dinning Room",
+name: "Conservatory",
+name: "Kitchen",
+name: "Study",
+name: "Library",
+name: "Billiard Room",
+name: "Lounge",
+name: "Ballroom",
+name: "Hall",
+name: "Spa",
+name: "Living Room",
+name: "Observatory",
+name: "Theater",
+name: "Guest House",
+name: "Patio"
 
-// Rooms' Collection
-var roomsArray = [];
+}];
 
-// Weapons Collection
-var weaponsArray = [];
+/*var myCards= [];
+function randomSelector (array){
+    if(array.length === 0){
+        return;
+    }else{
+    var myTargets= Math.floor(Math.random()* (charactersArray.length-1));
+    var myWeapons= Math.floor(Math.random()* (weaponsArray.length-1));
+    var myRooms= Math.floor(Math.random()* (roomsArray.length-1));
+    myCards.push({name: charactersArray[myTargets].name, weapon: weaponsArray[myWeapons].name, room: roomsArray[myRooms].name});
+    
+    return myCards[0];
+    }
+};*/
+
+var myCards= [];
+function randomSelector (array){
+    if(array.length === 0){
+        return;
+    }else{
+        var numeroRandom = Math.floor(Math.random() * (array.length-1));
+        return array[numeroRandom];
+    }
+    
+};
